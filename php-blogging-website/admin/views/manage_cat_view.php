27fc5d4e9dd5819517857d61066bd2d0a6acabb6
Harun <mharunurrashid777@gmail.com>
2023-03-19 00:28:59 +0600

    first commit

--- php-blogging-website/admin/views/manage_cat_view.php
+++ php-blogging-website/admin/views/manage_cat_view.php
@@ -4,5 +4,50 @@
 
 <h1 class="mt-4"><?php echo $pageTitle;?></h1>
 <ol class="breadcrumb mb-4">
+    <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
     <li class="breadcrumb-item active"><?php echo $pageTitle;?></li>
 </ol>
+
+<div class="row">
+    <div class="col-12">
+                    
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+                                All Categories
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Cat_ID</th>
+                                            <th>Category Name</th>
+                                            <th>Category Description</th>
+                                            <th>Action</th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Cat_ID</th>
+                                            <th>Category Name</th>
+                                            <th>Category Description</th>
+                                            <th>Action</th>
+                                            
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>61</td>
+                                            
+                                        </tr>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+    </div>
+</div>
