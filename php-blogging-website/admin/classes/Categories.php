f2170566bfa11a8d44ccef6f4e241872bdff0ac0
Harun <mharunurrashid777@gmail.com>
2023-03-20 09:02:13 +0600

    first commit

--- php-blogging-website/admin/classes/Categories.php
+++ php-blogging-website/admin/classes/Categories.php
@@ -1,6 +1,6 @@
 <?php
     class Categories extends Config{
-
+        // Add Categories
         public function add_cat($data){
          
            $cateName = $data['cat_name'];
@@ -11,9 +11,24 @@
           $result = $this->con->query($sql);
 
           if($result){
-            echo "ok";
+            $_SESSION['msg'] = "Added Successfully";
           
           }
+        }
+        //Display Category
+        public function display_cat(){
+
+          return  $this->con->query("SELECT * FROM `tbl_categories`;");
+        }
+
+        // Delete Category
+        public function delete_cat($cid){
+         $result = $this->con->query("DELETE FROM `tbl_categories` WHERE `cat_id`='$cid'");
+         if($result){
+              $_SESSION['msg'] = "Category Delete Successfully";
+              //header("Location: manage_category.php");
+         }
+
         }
     }
 
--- php-blogging-website/admin/views/manage_cat_view.php
+++ php-blogging-website/admin/views/manage_cat_view.php
@@ -1,16 +1,27 @@
-<?php
-    $pageTitle = "Manage Category";
-?>
+            <?php
+                $pageTitle = "Manage Category";
 
-<h1 class="mt-4"><?php echo $pageTitle;?></h1>
-<ol class="breadcrumb mb-4">
-    <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-    <li class="breadcrumb-item active"><?php echo $pageTitle;?></li>
-</ol>
+                include_once("classes/Categories.php");
+                $cat =  new Categories();
 
-<div class="row">
-    <div class="col-12">
-                    
+                if(isset($_GET['cat_id'])){
+                   $id = $_GET['cat_id'];
+
+                   $cat->delete_cat($id);
+                }
+
+                
+            ?>
+
+            <h1 class="mt-4"><?php echo $pageTitle;?></h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                <li class="breadcrumb-item active"><?php echo $pageTitle;?></li>
+            </ol>
+
+            <div class="row">
+                <div class="col-12">
+                                
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
@@ -37,17 +48,32 @@
                                         </tr>
                                     </tfoot>
                                     <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
+                                        <?php
+                                            $data = $cat->display_cat();
+                                            $i= 1;
+                                            if(mysqli_num_rows($data)>0){
+
+                                                foreach($data as $catitem){?>
+                                                
+                                                <tr>
+                                                    <td><?php echo $i++;?></td>
+                                                    <td><?php echo $catitem['cat_name'];?></td>
+                                                    <td><?php echo $catitem['cat_des'];?></td>
+                                                    <td>
+                                                        <a href="?cat_id=<?php echo $catitem['cat_id'];?>" class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></a>
+                                                    </td>
                                             
-                                        </tr>
+                                                </tr>
+                                        
+                                                <?php    
+                                                }
+                                            }
+                                        ?>
                                         
                                     </tbody>
                                 </table>
                             </div>
                         </div>
-    </div>
-</div>
+
+                </div>
+            </div>
